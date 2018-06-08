c09f2308d8496f63e83185be05c68c6614a8e67b
nastyasorokina <37139309+nastyasorokina@users.noreply.github.com>
2018-06-08 20:22:49 +0300

    Rename lab1.h to matrix.h

---
+++ matrix.h
@@ -0,0 +1,38 @@
+#ifndef LAB1_HEADER_H
+#define LAB1_HEADER_H
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Matrix
+{
+public:
+    Matrix (int x = 3, int y = 3);
+    Matrix (Matrix &);// конструктор
+    ~Matrix();//  деструктор
+    int Get_Width() ;// получение ширины
+    int Get_Length();// получение длины
+    int ** Get_Array();
+    void transpose();// функция транспорирования
+
+    Matrix & operator = (const Matrix & );
+    Matrix & operator *=(const int & );
+    Matrix & operator +=(const Matrix &);
+    Matrix & operator -=(const Matrix & );
+    Matrix operator +(const Matrix & );
+    Matrix operator -(const Matrix & );
+    Matrix operator -();
+    Matrix operator *(const Matrix & );
+    Matrix & operator++();
+    Matrix & operator++(int);
+
+
+private:
+    int width, length;
+    int ** array;
+};
+
+ifstream & operator >> (ifstream &, Matrix &);
+ofstream & operator << (ofstream &, Matrix &);
+#endif //LAB2_HEADER_H
